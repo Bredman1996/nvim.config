fd2b97d13c3c82d00f93ada725a3024534efd456
Brandon Redman <bredman1996@gmail.com>
2026-04-02 21:38:45 -0500

    some minor updates

--- init.lua
+++ init.lua
@@ -94,6 +94,9 @@ vim.keymap.set("n", "<leader>`", "<cmd>horizontal terminal<CR>", { desc = "open 
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
+vim.keymap.set("n", "<leader>Tn", "<cmd>+tabnext<CR>", { desc = "Next Tab" })
+vim.keymap.set("n", "<leader>Tp", "<cmd>-tabnext<CR>", { desc = "Previous Tab" })
+
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- lua/plugins/web-search.lua
+++ lua/plugins/web-search.lua
@@ -1,13 +1,14 @@
 return {
-  'bredman1996/web-search.nvim',
-  -- dir = '~/source/web-search.nvim',
-  config = function()
-    require('web-search').setup {
-      sourceMaps = {},
-    }
-    vim.keymap.set('n', '<leader>wS', '<cmd>WebSearch<CR>', { desc = 'WebSearch Prompt' })
-    vim.keymap.set('v', '<leader>wS', '<cmd>WebSearchSelection<CR>', { desc = 'WebSearch Search Highlighted' })
-    vim.keymap.set('v', '<leader>wt', '<cmd>WebSearchTerraform<CR>', { desc = 'WebSearch Search Terraform' })
-    vim.keymap.set('v', '<leader>wa', '<cmd>WebSearchAdo<CR>', { desc = 'WebSearch Search AzureDevops Tasks' })
-  end,
+	"bredman1996/web-search.nvim",
+	--	dir = "~/source/web-search.nvim",
+	config = function()
+		require("web-search").setup({
+			sourceMaps = {},
+			searchEngine = "duckduckgo",
+		})
+		vim.keymap.set("n", "<leader>wS", "<cmd>WebSearch<CR>", { desc = "WebSearch Prompt" })
+		vim.keymap.set("v", "<leader>wS", "<cmd>WebSearchSelection<CR>", { desc = "WebSearch Search Highlighted" })
+		vim.keymap.set("v", "<leader>wt", "<cmd>WebSearchTerraform<CR>", { desc = "WebSearch Search Terraform" })
+		vim.keymap.set("v", "<leader>wa", "<cmd>WebSearchAdo<CR>", { desc = "WebSearch Search AzureDevops Tasks" })
+	end,
 }
